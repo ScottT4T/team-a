2e5c084689261d0f4ed3a7e822298fec2fee1dc8
Adrian Jones <adrian.jones2@vodafone.com>
2022-11-14 17:41:05 +0000

    FE connecting to BE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//
+//
+// Response {type: 'basic', url: 'http://localhost/api/hello', redirected: false, status: 200, ok: true, …}
+//
 function App() {
+  const [data, setdata] = useState<any>()
   const requestJSON = async (requestPath: any): Promise<any> => {
-    const response = await fetch(requestPath)
+    const response = await fetch(requestPath, {method:"GET"})
       .then((res?: Response) => {
-        console.log(res)
+        res?.json().then((response) => {setdata(response)})
       })
       .catch(console.error)
-  
     return response
   }
 
@@ -17,16 +21,30 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <button
+          onClick={() => {
+            window.location.href="http://localhost:3333/hello"
+          }}
+          >
+          Test Server (http://localhost:3333/hello)
+        </button>
+        <br/>
+        <button
+          onClick={() => {
+            window.location.href="http://localhost/api/hello"
+          }}
+        >
+          Test API (http://localhost/api/hello)
+        </button>
+        <br/>
         <button
           onClick={() => {
             requestJSON('/api/hello')
           }}
         >
-          Learn React
+          Test UI (fetch /api/hello)
         </button>
+        <div id="response">{JSON.stringify(data)}</div>
       </header>
     </div>
   );
