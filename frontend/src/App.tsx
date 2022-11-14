6c43ffc6af0f75d23f40448b876b52e581d43093
Scott Addinall <saddinall1@gmail.com>
2022-11-14 16:44:48 +0000

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AnyRecord } from 'dns';
 
 function App() {
   const requestJSON = async (requestPath: any): Promise<any> => {
