420a7faa3c6857a146379c9c9bc7e86864f31950
Scott Addinall <saddinall1@gmail.com>
2022-11-22 13:13:12 +0000

    API requests formats

--- frontend/src/typescript/Components/Requests/getHighscores.ts
+++ frontend/src/typescript/Components/Requests/getHighscores.ts
@@ -1,5 +1,7 @@
-const getHighscores = () => {
+import { HTTPMethods } from "../../RequestJson/Constants"
+import requestJSON from "../../RequestJson/RequestJson"
+import { Highscores } from "../../Types"
 
-}
+const getHighscores = async (): Promise<Highscores> => await requestJSON('/api/getHighscores', HTTPMethods.GET)
 
 export default getHighscores
