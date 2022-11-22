b0e537d14f24c4c0e8a4cafa3c13d925abe446ce
Scott Addinall <saddinall1@gmail.com>
2022-11-22 14:19:13 +0000

    Update ratePet.ts

--- frontend/src/typescript/Components/Requests/ratePet.ts
+++ frontend/src/typescript/Components/Requests/ratePet.ts
@@ -2,6 +2,6 @@ import { HTTPMethods } from "../../RequestJson/Constants"
 import requestJSON from "../../RequestJson/RequestJson"
 import { Pet } from "../../Types"
 
-const getPets = async (petId: number): Promise<Pet[]> => await requestJSON(`/api/ratePet?id=${petId}`, HTTPMethods.POST)
+const getPets = async (petId: number, like: boolean): Promise<Pet[]> => await requestJSON(`/api/ratePet?id=${petId}&like=${like}`, HTTPMethods.POST)
 
 export default getPets
