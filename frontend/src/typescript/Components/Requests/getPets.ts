bc6253a78419c923db5c5eed38ee53eba7ccf6f9
Scott Addinall <saddinall1@gmail.com>
2022-11-22 13:19:10 +0000

    Update getPets.ts

--- frontend/src/typescript/Components/Requests/getPets.ts
+++ frontend/src/typescript/Components/Requests/getPets.ts
@@ -2,6 +2,6 @@ import { HTTPMethods } from "../../RequestJson/Constants"
 import requestJSON from "../../RequestJson/RequestJson"
 import { Pet } from "../../Types"
 
-const getPets = async (): Promise<Pet[]> => await requestJSON('/api/getHighscores', HTTPMethods.GET)
+const getPets = async (): Promise<Pet[]> => await requestJSON('/api/getPets', HTTPMethods.GET)
 
 export default getPets
